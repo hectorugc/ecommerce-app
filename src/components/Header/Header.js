15c59dfb1e09fc814bf926b39f6ceb6d83a8c6b3
Hector Cordova <comptetest@MacBook-Pro-de-Compte.local>
2021-09-22 18:02:46 -0500

    Created basic frontend for the sneakers store

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import {FaShoppingCart} from 'react-icons/fa'
+import Container from '@components/Container'
+import styles from './Header.module.scss'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <Container className={styles.headerContainer}>
+            <p className={styles.headerTitle}>Shoesh&apos; Snkrs</p>
+                <p className={styles.headerCart}><FaShoppingCart/> 0.0</p>
+            </Container>
+        </header>
+    )
+}
+
+export default Header
