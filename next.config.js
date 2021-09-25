9c1de2695bd9e8e6b7b6b9a55d1ff2d7397ab19a
hector ulises <hectorugc4@gmail.com>
2021-09-24 20:38:22 -0500

    helo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   target:'serverless',
   images: {
-    domains: [process.env.NEXT_IMAGE_ALLOWED_DOMAINS],
+    domains: ['ecommercenext.local/'],
   }
 }
